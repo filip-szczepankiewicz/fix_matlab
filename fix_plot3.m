f5ca0f948f601e52bba02da775a80a63384cb943
FilipSz <filip.szczepankiewicz@gmail.com>
2024-09-05 16:05:32 +0200

    Bugfix

--- fix_plot3.m
+++ fix_plot3.m
@@ -1,5 +1,5 @@
 function h = fix_plot3(u)
 % function h = fix_plot3(u)
 
-h = plot3(u(:,1), u(:,2), u(:,3), 'k.');
+h = plot3(u(:,1), u(:,2), u(:,3), 'k-');
 
--- fix_ticklabels.m
+++ fix_ticklabels.m
@@ -11,8 +11,11 @@ function t = fix_ticklabels(ax_str, tno, prec, ax_h)
 % for the x and y-axes.
 
 if nargin < 1
-    fix_my_ticklabels('x');
-    fix_my_ticklabels('y');
+    fix_ticklabels('x');
+    fix_ticklabels('y');
+    try
+        fix_ticklabels('z');
+    end
     return
 end
 
@@ -31,45 +34,45 @@ end
 
 
 if nargin < 3 || isempty(prec)
-    
+
     t_labs = get(ax_h, curr_lab);
     t = cellfun(@str2num,t_labs);
-    
+
     prec = 0;
-    
+
     for i = 1:size(t_labs,1)
-        
+
         tmp_str = t_labs{i,:};
-        
+
         p_ind = strfind(tmp_str, '.');
-        
+
         if ~isempty(p_ind)
-            
+
             tmp_prec = length(tmp_str) - p_ind;
-            
+
             if tmp_prec > prec
                 prec = tmp_prec;
             end
-            
+
         end
-        
+
     end
-    
-    
+
+
 else
-    
+
     if prec ~= round(prec); error('Precision must be an integer!'); end
-    
+
     tlims = get(ax_h, curr_lim);
-    
+
     d = round(range(tlims)/tno*10^prec)/10^prec;
-    
+
     if d == 0; d = ceil(range(tlims)/tno*10^prec)/10^prec; end
-    
+
     tstart = round(min(tlims)*10^prec)/10^prec;
-    
+
     t = (tstart:d:max(tlims))';
-    
+
 end
 
 set(ax_h, curr_ax, t,curr_lab, cellstr( num2str(t, [' %01.' num2str(max([0 prec])) 'f']) )  )
